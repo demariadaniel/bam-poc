e4200c44f686d17ff44b77163dd4549fa27fa0a4
Dan DeMaria <ddemaria@oicr.on.ca>
2024-04-22 16:59:27 -0400

    State Management - Local Storage solution

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -17,28 +19,157 @@ import socket from "bam.iobio/client/js/socket.io.js";
 
 import build from "bam.iobio/client/dist/build.js";
 
+const defaultBamContext = {
+  pieChooser: true,
+  readCoverageBox: true,
+  readsSampledBox: true,
+  mappedReads: true,
+  forwardStrands: true,
+  properPairs: true,
+  singletons: true,
+  bothMatesMapped: true,
+  duplicates: true,
+  mappingQualityDistribution: true,
+};
+
 function App() {
   const bamUrl =
     "/?bam=https://s3.amazonaws.com/iobio/NA12878/NA12878.autsome.bam";
+
   const fileLoaded = Boolean(window.location.search);
+
+  const localBamConfig = JSON.parse(localStorage.getItem("bamConfig"));
+
+  const [bamContext, setBamContext] = useState(
+    localBamConfig || defaultBamContext
+  );
+
+  const {
+    pieChooser,
+    readCoverageBox,
+    readsSampledBox,
+    mappedReads,
+    forwardStrands,
+    properPairs,
+    singletons,
+    bothMatesMapped,
+    duplicates,
+    mappingQualityDistribution,
+  } = bamContext;
+
+  const updateContext = (key, value) => {
+    const newContext = {
+      ...bamContext,
+      [key]: !value,
+    };
+    setBamContext(newContext);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("bamConfig", JSON.stringify(bamContext));
+  }, [bamContext]);
 
   return (
     <div className="App">
       <header className="App-header">
-        {!fileLoaded && (
-          <>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a href={bamUrl}>Demo URL</a>
-          </>
-        )}
         <>
-          {fileLoaded && (
+          {fileLoaded ? (
             <a className="Back-button" href={"/"}>
               Back
             </a>
+          ) : (
+            <>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a href={bamUrl}>Demo URL</a>
+              <div style={{ margin: "15px" }}>
+                <button
+                  className={pieChooser ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("pieChooser", pieChooser);
+                  }}
+                >
+                  Pie Chooser
+                </button>
+                <button
+                  className={readCoverageBox ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("readCoverageBox", readCoverageBox);
+                  }}
+                >
+                  Read Coverage
+                </button>
+                <button
+                  className={readsSampledBox ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("readsSampledBox", readsSampledBox);
+                  }}
+                >
+                  Reads Sampled
+                </button>
+                <button
+                  className={mappedReads ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("mappedReads", mappedReads);
+                  }}
+                >
+                  Mapped Reads
+                </button>
+                <button
+                  className={forwardStrands ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("forwardStrands", forwardStrands);
+                  }}
+                >
+                  Forward Strands
+                </button>
+                <button
+                  className={properPairs ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("properPairs", properPairs);
+                  }}
+                >
+                  Proper Pairs
+                </button>
+                <button
+                  className={singletons ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("singletons", singletons);
+                  }}
+                >
+                  Singletons
+                </button>
+                <button
+                  className={bothMatesMapped ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("bothMatesMapped", bothMatesMapped);
+                  }}
+                >
+                  Both Mates Mapped
+                </button>
+                <button
+                  className={duplicates ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext("duplicates", duplicates);
+                  }}
+                >
+                  Duplicates
+                </button>
+                <button
+                  className={mappingQualityDistribution ? "button-active" : ""}
+                  onClick={() => {
+                    updateContext(
+                      "mappingQualityDistribution",
+                      mappingQualityDistribution
+                    );
+                  }}
+                >
+                  Mapping Quality Distribution
+                </button>
+              </div>
+            </>
           )}
           <div id="app"></div>
           <script src={bamClass}></script>
